70dc3f81ac42d05dccb59ba934dc83ff082173e1
Felipe Leonardo Gómez-Cortés <flgomezc87@gmail.com>
2022-07-12 16:18:10 +0200

    Create dbs2go_DBS3_file_revalidation.py

---
+++ scripts/dbs2go_DBS3_file_revalidation.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+from optparse import OptionParser
+import logging
+import os
+import sys
+
+from urls import *
+
+from dbs.apis.dbsClient import DbsApi
+from dbs.exceptions.dbsClientException import dbsClientException
+
+dbsApi = DbsApi(url=TEST_url) # `TEST_url` or `url`
+
+def main():
+    usage="%prog <options>"
+    parser = OptionParser(usage=usage)
+
+    parser.add_option("-f", "--file", dest="lfn",
+                      help="file to make VALID again",
+                      metavar="<lfn or filename>")
+    #set default values
+    parser.set_defaults(lfn=0)
+    (opts, args) = parser.parse_args()
+
+    fstatus=1
+    lost=0
+    result=0
+    lfn=0
+
+    if opts.lfn:
+        try:
+            lfn=opts.lfn
+            result = dbsApi.updateFileStatus(logical_file_name=lfn, is_file_valid=fstatus, lost=lost)
+            print(result)
+        except:
+            lfn=0
+    if result == []:
+        print("RE-Validation OK for:", lfn)
+    else:
+        print("FAILED for:", lfn)
+
+if __name__ == "__main__":
+    main()
