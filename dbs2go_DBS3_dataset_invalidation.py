df2d2892cce650f0bf445118b01032cca366bc26
Felipe Gomez <flgomezc87@gmail.com>
2023-11-15 14:07:59 +0100

    remove extra-spaces, added Connection error exception.

--- dbs2go_DBS3_dataset_invalidation.py
+++ dbs2go_DBS3_dataset_invalidation.py
@@ -1,57 +1,66 @@
 # This script cannot recognize if a dataset exists or not.
 # Python 3.8.2
-from optparse import OptionParser                                                                                                           
-import logging                                                                                                                              
-import os                                                                                                                                   
-import sys                                                                                                                                  
-                                                                                                                                            
-from urls import *                                                                                                                          
-                                                                                                                                            
-from dbs.apis.dbsClient import DbsApi                                                                                                       
-from dbs.exceptions.dbsClientException import dbsClientException                                                                            
-                                                                                                                                            
-                                                                                                                                            
-dbsApi = DbsApi(url=url) # 'TEST_URL' or just 'url'                                                                                         
-                                                                                                                                            
-FILEVALID = 1                                                                                                                               
-FILEINVALID = 0                                                                                                                             
-VALID   = 1                                                                                                                                 
-INVALID = 0                                                                                                                                 
-                                                                                                                                            
-lost = 0                                                                                                                                    
-def main():                                                                                                                                 
-    usage="%prog <options>"                                                                                                                 
-    parser = OptionParser(usage=usage)                                                                                                      
-                                                                                                                                            
-    parser.add_option("-d", "--dataset", dest="dataset",                                                                                    
-                      help="file be invalidated",                                                                                           
-                      metavar="<lfn or filename>")                                                                                          
-    #set default values                                                                                                                     
-    parser.set_defaults(lfn=0)                                                                                                              
-    (opts, args) = parser.parse_args()                                                                                                      
-                                                                                                                                            
-    if opts.dataset:                                                                                                                        
-        try:                                                                                                                                
-            dataset=opts.dataset                                                                                                            
-            listFiles = dbsApi.listFiles(dataset=dataset, detail=True)                                                                      
-            n_files = len(listFiles)                                                                                                        
-                                                                                                                                            
-            file_invalidation    = dbsApi.updateFileStatus(dataset=dataset, is_file_valid=FILEINVALID, lost=lost) #INVALIDATE just files in\
- dataset, not recursive                                                                                                                     
-            dataset_invalidation = dbsApi.updateDatasetType(dataset=dataset, dataset_access_type="INVALID")  # INVALID DELETED DEPRECATED
-                                                                                                                                            
-        except:                                                                                                                             
-            result = 0                                                                                                                      
-            print("ERROR: Failed to invalidate", dataset)                                                                                   
-                                                                                                                                            
-        if (dataset_invalidation == []) and (file_invalidation == []):                                                                      
-            print("INVALIDATION OK: Dataset and files are invalid. {} files in {}".format(n_files, dataset))                                
-        else:                                                                                                                               
-            if(dataset_invalidation != []):                                                                                                 
-                print("ERROR: cannot update dataset status of ", dataset)                                                                   
-                                                                                                                                            
-            if(file_invalidation != []):                                                                                                    
-                print("ERROR: cannot update file status for files in ", dataset)                                                            
-                                                                                                                                            
-if __name__ == "__main__":                                                                                                                  
-    main()    
+import argparse
+import logging
+import os
+import sys
+
+from urls import *
+
+from dbs.apis.dbsClient import DbsApi
+from dbs.exceptions.dbsClientException import dbsClientException
+
+
+dbsApi = DbsApi(url=url) # 'TEST_URL' or just 'url'
+
+# DBS Constants
+FILEVALID = 1
+FILEINVALID = 0
+VALID   = 1
+INVALID = 0
+DATASETINVALID = "INVALID"
+LOST = 0
+
+def main():
+    """
+    Invalidates Dataset(PheDex)/Container(Rucio) on DBS using the pbs2go api
+    """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-d", "--dataset", dest="dataset", type=str,
+                      help="Introduce the Dataset(PheDex)/Container(Rucio) to be invalidated",
+                      metavar="<CMS container>")
+
+    parser.parse_args(args=None if sys.argv[1:] else ['--help'])
+
+    try:
+        args = parser.parse_args()
+    except ValueError:
+        parser.print_help()
+        sys.exit(0)
+
+    if args.dataset:
+        try:
+            dataset=args.dataset
+            list_files = dbsApi.listFiles(dataset=dataset, detail=True)
+            n_files = len(list_files)
+            file_invalidation = dbsApi.updateFileStatus(dataset=dataset, 
+                                                        is_file_valid=FILEINVALID, lost=LOST)
+            if file_invalidation == []:
+                print(f"    INVALIDATION OK: {n_files} files invalidated in {dataset}")
+            dataset_invalidation = dbsApi.updateDatasetType(dataset=dataset,
+                                                            dataset_access_type=DATASETINVALID)
+            if (dataset_invalidation == []) and (file_invalidation == []):
+                print(f"DATASET INVALIDATION OK: {dataset}")
+
+        except ConnectionError:
+            print("ERROR: Failed to invalidate files or dataset ", dataset)
+            logging.error('Failed to invalidate files or dataset')
+
+        if dataset_invalidation != []:
+            print(f"ERROR: cannot update dataset status of {dataset}")
+        if file_invalidation != []:
+            print(f"ERROR: cannot update file status for files in {dataset}")
+
+if __name__ == "__main__":
+    main()
